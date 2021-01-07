557bbceef0b90741ffa71bacc51b7f8c4eadbffd
jeldikk <jeldi.kamal2013@gmail.com>
2021-01-07 22:49:45 +0530

    Added header component with sass file for styling

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {Navbar, Nav, Container} from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Paginate Commits</Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
