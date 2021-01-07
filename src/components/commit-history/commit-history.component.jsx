b5704c39821a5cc68f4ad1288b042e386b63f19f
jeldikk <jeldi.kamal2013@gmail.com>
2021-01-07 22:52:45 +0530

    added commit-history component and sass file, it takes a list of commits and displays them

---
+++ src/components/commit-history/commit-history.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./commit-history.styles.scss";
+
+import CommitDetails from "../commit-details/commit-details.component";
+
+function CommitHistory({ commitList }) {
+    // console.log({commitList})
+  return (
+    <div className="commit-history">
+      {
+        commitList.length === 0 
+        ?
+        <div className="empty-data-text">
+            <div className="title">Here goes the title</div>
+            <div className="description">Here goes the description</div>
+            <div className="howto">How to Use it</div>
+        </div>
+        : 
+        commitList.map((commit) => <CommitDetails key={commit.sha} commitObj={commit} />)
+      }
+    </div>
+  )
+}
+
+export default CommitHistory;
