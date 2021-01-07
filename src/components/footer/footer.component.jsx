29d10a152475ffbe0d295983a32161ff0401de99
jeldikk <jeldi.kamal2013@gmail.com>
2021-01-07 22:50:43 +0530

    Added footer component and integrated as a bottom sticky footer

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./footer.styles.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer text-center fixed-bottom">
+            Made with Love on React Bootstrap axios
+        </div>
+    )
+}
+
+export default Footer
