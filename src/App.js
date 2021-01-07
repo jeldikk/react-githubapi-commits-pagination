4825913f953f51d92a4b7ec55f13952afce8e8d6
jeldikk <jeldi.kamal2013@gmail.com>
2021-01-07 22:57:58 +0530

    integrated header, footer, input-form, commit-history, commit-details and pagination to App. implemented basic data fetch on updation of owner and repo name after fetchdata button click. Boundary conditions are yet to be tested

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container } from 'react-bootstrap'
+import ReactPaginate from "react-paginate"
+
+import Header from './components/header/header.component'
+import Footer from "./components/footer/footer.component"
+import InputForm from "./components/input-form/input-form.component"
+import CommitHistory from "./components/commit-history/commit-history.component"
+import Pagination from "./components/pagination/pagination.component"
+// import axios from 'axios';
+
+
+
+import { fetchData } from "./axios/axios.util"
+
+class App extends React.Component {
+
+  state = {
+    isLoading: false,
+    owner: '',
+    repo: '',
+    data: [],
+    per_page: 10,
+    page_num: 1,
+    totalItemsCount: null
+  }
+
+  componentDidMount() {
+
+    // fetchData('django','django',10,1)
+    //   .then((response) => this.setState({data: response.data}))
+
+  }
+
+  updateCommitData = (infoObject) => {
+
+    this.setState({ isLoading: true })
+    console.log({ infoObject })
+    const { owner, repo, per_page, page_num } = infoObject;
+    console.log("Execution came here")
+    fetchData(owner, repo, per_page, page_num)
+      .then((response => {
+        if (response.status === 200) {
+
+          this.setState({
+            data: response.data
+          })
+        }
+      }))
+  }
+
+  updateInputDetails = ({ owner, repo }) => {
+
+    console.log({ owner, repo })
+    this.setState((prevState, prevProps) => {
+      return {
+        ...prevState,
+        owner,
+        repo
+      }
+    }, () => this.updateCommitData(this.state))
+
+  }
+
+  onPageChange = (pagenumber) => {
+    console.log("The page number is ", pagenumber)
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <Container>
+          <InputForm updateInputDetails={this.updateInputDetails} />
+          <CommitHistory commitList={this.state.data} />
+          <Pagination pageCount={2} pageRangeDisplayed={5} marginPagesDisplayed={5} onPageChange={this.onPageChange}/>
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
+
 }
 
 export default App;
