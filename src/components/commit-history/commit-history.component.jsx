b18da78a677e3b929752ced627f35926cd5daf52
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-06 12:48:43 +0530

    Added title, description and how to use on empty commit list or errored commit search query

--- src/components/commit-history/commit-history.component.jsx
+++ src/components/commit-history/commit-history.component.jsx
@@ -12,9 +12,22 @@ function CommitHistory({ commitList, errorMessage }) {
         commitList.length === 0 
         ?
         <div className="empty-data-text">
-            <div className="title">Here goes the title</div>
-            <div className="description">Here goes the description</div>
-            <div className="howto">How to Use it</div>
+            <h1 className="title">Github commits history viewer</h1>
+            <div className="description">
+              This is simple react web application which avails github public repository commit history api to retrieve the commits and displays them with a pagination.
+            </div>
+            <hr />
+            <br />
+            <div className="howto">
+              <h2>How to Use</h2>
+              <div className="description">
+                let say for example try owner name with <code>jeldikk</code>, and repository name <code>ismartcsv</code>. You will be displayed with a loading spinner displaying the commits of <code>jeldikk/ismartcsv</code> in a paginated manner.
+                <br />
+                You can even try with owner <code>django</code>, and repository name <code>django</code> to look overs commits made in <code>django/django</code> open source python webframework project
+              </div>
+              <hr />
+              <p className="text-danger">If there is any error in loading commit history, it will print the same intro message on the page</p>
+            </div>
         </div>
         : 
         commitList.map((commit) => <CommitDetails key={commit.sha} commitObj={commit} />)
