7e1e388ff375b7d74bbe348c4220cc2edc369c33
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-06 12:46:26 +0530

    avoided property non-existance problem using optional chaining to access author login name

--- src/components/commit-details/commit-details.component.jsx
+++ src/components/commit-details/commit-details.component.jsx
@@ -6,8 +6,13 @@ import "./commit-details.styles.scss"
 
 function CommitDetails({commitObj}) {
 
-    const {commit:{message},author:{login}} = commitObj;
-    console.log(commitObj)
+    // const {commit:{message},author:{login}} = commitObj;
+    // console.log(commitObj)
+
+    //optional chaining operator to avoid non-existing property problem
+    const message = commitObj?.commit?.message;
+    const login = commitObj?.author?.login;
+    console.log({message, login})
 
     return (
         <Card className="commit-details shadow-lg">
